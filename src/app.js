d99deca527239034637f8e0025f2dacb3f1a1485
thisisannak23 <anna.kazanecki@gmail.com>
2022-04-21 21:04:12 -0500

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let currentTemp = document.querySelector("#currentTemp");
@@ -5,12 +17,13 @@ function displayTemperature(response) {
   let humidityPercent = document.querySelector("#humidityPercent");
   let windSpeedMPH = document.querySelector("#windSpeedMPH");
   let currentDescrip = document.querySelector("#currentDescrip");
-
+  let dayTime = document.querySelector("#dayTime");
   currentTemp.innerHTML = Math.round(response.data.main.temp);
   currentCity.innerHTML = (response.data.name);
   humidityPercent.innerHTML = Math.round(response.data.main.humidity);
   windSpeedMPH.innerHTML = Math.round(response.data.wind.speed);
   currentDescrip.innerHTML = (response.data.weather[0].description);
+  dayTime.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
