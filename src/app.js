ffa6ae7815272927e4871b3483f4431437869375
thisisannak23 <anna.kazanecki@gmail.com>
2022-04-30 22:57:34 -0500

    added search form and made it work

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
@@ -17,23 +18,31 @@ function displayTemperature(response) {
   let windSpeedMPH = document.querySelector("#windSpeedMPH");
   let currentDescrip = document.querySelector("#currentDescrip");
   let dayTime = document.querySelector("#dayTime");
-  //let currentPic = document.querySelector("#currentPic");
-
+  let currentPic = document.querySelector("#currentPic");
 
   currentTemp.innerHTML = `${Math.round(response.data.main.temp)}°`;
-  currentCity.innerHTML = (response.data.name);
+  currentCity.innerHTML = response.data.name;
   humidityPercent.innerHTML = Math.round(response.data.main.humidity);
   windSpeedMPH.innerHTML = Math.round(response.data.wind.speed);
   currentDescrip.innerHTML = (response.data.weather[0].description);
   dayTime.innerHTML = formatDate(response.data.dt * 1000);
-  //currentPic.innerHTML = `${response.data.}`;
+  currentPic.setAttribute = ("src", `src/${response.data.weather[0].icon}d.png`);
+  currentPic.setAttribute = ("alt", response.data.weather[0].description);
 }
 
-let apiKey = "f9de746b9d23a9c915974277fc1710ae";
-let city = "Chicago"
-let currentApiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+  let apiKey = "f9de746b9d23a9c915974277fc1710ae";
+  let currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(currentApiUrl).then(displayTemperature);
+}
 
-axios.get(currentApiUrl).then(displayTemperature);
+function citySearch(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#searchBar");
+  search(cityInput.value);
+}
 
-currentTemp
+search("St. Louis");
+
+let form = document.querySelector("#searchAndButton");
+form.addEventListener("submit", citySearch);
